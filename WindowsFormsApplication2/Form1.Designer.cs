5360aca4d94b2cc147394a2375b4315604689f49
Unknown <ltockner@outlook.com>
2018-01-23 16:58:40 +0100

    Added everything
    Everything has been added. Please forgive the naming, I forgot to change it when creating the project

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofd_button = new System.Windows.Forms.Button();
+            this.loaded_file_display = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.file_length = new System.Windows.Forms.TextBox();
+            this.spaces_display = new System.Windows.Forms.TextBox();
+            this.show_frequenzy = new System.Windows.Forms.Button();
+            this.pBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // ofd_button
+            // 
+            this.ofd_button.Location = new System.Drawing.Point(12, 12);
+            this.ofd_button.Name = "ofd_button";
+            this.ofd_button.Size = new System.Drawing.Size(83, 23);
+            this.ofd_button.TabIndex = 0;
+            this.ofd_button.Text = "Open file";
+            this.ofd_button.UseVisualStyleBackColor = true;
+            this.ofd_button.Click += new System.EventHandler(this.ofd_button_Click);
+            // 
+            // loaded_file_display
+            // 
+            this.loaded_file_display.Location = new System.Drawing.Point(101, 14);
+            this.loaded_file_display.Name = "loaded_file_display";
+            this.loaded_file_display.ReadOnly = true;
+            this.loaded_file_display.Size = new System.Drawing.Size(191, 20);
+            this.loaded_file_display.TabIndex = 1;
+            this.loaded_file_display.Text = "Loaded file: none";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 42);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(280, 23);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Analyze";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // file_length
+            // 
+            this.file_length.Location = new System.Drawing.Point(13, 72);
+            this.file_length.Name = "file_length";
+            this.file_length.ReadOnly = true;
+            this.file_length.Size = new System.Drawing.Size(159, 20);
+            this.file_length.TabIndex = 3;
+            this.file_length.Text = "Total signs in file: ";
+            // 
+            // spaces_display
+            // 
+            this.spaces_display.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.spaces_display.Location = new System.Drawing.Point(178, 71);
+            this.spaces_display.Name = "spaces_display";
+            this.spaces_display.ReadOnly = true;
+            this.spaces_display.Size = new System.Drawing.Size(114, 20);
+            this.spaces_display.TabIndex = 4;
+            this.spaces_display.Text = "Spaces: ";
+            // 
+            // show_frequenzy
+            // 
+            this.show_frequenzy.Enabled = false;
+            this.show_frequenzy.Location = new System.Drawing.Point(13, 98);
+            this.show_frequenzy.Name = "show_frequenzy";
+            this.show_frequenzy.Size = new System.Drawing.Size(279, 23);
+            this.show_frequenzy.TabIndex = 5;
+            this.show_frequenzy.Text = "Show letter frequency";
+            this.show_frequenzy.UseVisualStyleBackColor = true;
+            this.show_frequenzy.Click += new System.EventHandler(this.show_frequenzy_Click);
+            // 
+            // pBar1
+            // 
+            this.pBar1.Enabled = false;
+            this.pBar1.Location = new System.Drawing.Point(13, 128);
+            this.pBar1.Name = "pBar1";
+            this.pBar1.Size = new System.Drawing.Size(279, 15);
+            this.pBar1.Step = 1;
+            this.pBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 148);
+            this.Controls.Add(this.pBar1);
+            this.Controls.Add(this.show_frequenzy);
+            this.Controls.Add(this.spaces_display);
+            this.Controls.Add(this.file_length);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.loaded_file_display);
+            this.Controls.Add(this.ofd_button);
+            this.Name = "Form1";
+            this.Text = "Text Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ofd_button;
+        private System.Windows.Forms.TextBox loaded_file_display;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox file_length;
+        private System.Windows.Forms.TextBox spaces_display;
+        private System.Windows.Forms.Button show_frequenzy;
+        private System.Windows.Forms.ProgressBar pBar1;
+
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using JR.Utils.GUI.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        //Variables, custom font, etc. 
+        String[] to_exclude = { ",", ".", ":", "-", "\r\n", "=", " ", "(", ")", '"'.ToString(), "<", ">", };
+        OpenFileDialog ofd = new OpenFileDialog();
+        String file_contents;
+        bool file_loaded = false;
+        Font font = new Font(
+            "Courier New",
+            10, 
+            FontStyle.Regular,
+            GraphicsUnit.Point
+            );
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //For loading in the file itself
+        private void ofd_button_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            ofd.InitialDirectory = "c:\\";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((ofd.OpenFile()) != null)
+                    {
+                        using (StreamReader sr = new StreamReader(ofd.FileName))
+                        {
+                            String text = sr.ReadToEnd();
+                            file_contents = text;
+                            file_loaded = true;
+                            loaded_file_display.Text = "Loaded file: " + ofd.SafeFileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        //This is what happens if you hit "Analyze"
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (file_loaded)
+            {
+                file_length.Text = "Total signs in file: " + file_contents.Length.ToString();
+                spaces_display.Text = "Spaces: " + file_contents.Split(' ').Length;
+                show_frequenzy.Enabled = true;
+                pBar1.Enabled = true;
+                pBar1.Maximum = file_contents.Length;
+            }
+            else
+            {
+                MessageBox.Show("Error: Load file first");
+            }
+        }
+
+        //Returns the total amount of times a letter has been used in the input file
+        internal Letter[] get_letter_frequenzy (String input) 
+        {
+            List<Letter> letters = new List<Letter>();
+            try
+            {
+                letters.Add(new Letter() { letter = input[0], count = 1 });
+                for ( int i = 1; i < input.Length; i++) {
+                    bool need_to_add = true;
+                    foreach (Letter l in letters)
+                    {
+                        if (to_exclude.Contains(input[i].ToString())) //Excludes any Strings from the exclude array
+                        {
+                            need_to_add = false;
+                            continue;
+                        }
+                        if (input[i] == l.letter)
+                        {
+                            l.count++;
+                            need_to_add = false;
+                        }
+                    }
+                    if (need_to_add)
+                    {
+                        letters.Add(new Letter { letter = input[i], count = 1 });
+                    }
+                    pBar1.PerformStep();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Analysis failed. Error Code: " + e);
+            }
+            letters.Sort((x, y) => y.count.CompareTo(x.count));
+            return letters.ToArray();
+        }
+
+        private void show_frequenzy_Click(object sender, EventArgs e)
+        {
+            FlexibleMessageBox.FONT = font;
+            FlexibleMessageBox.Show(build_text(get_letter_frequenzy(file_contents.ToLower())));
+            pBar1.Value = 0;
+        }
+
+        // Generates the text which will be displayed in the final Message box
+        private String build_text (Letter[] input)
+        {
+            String return_text = "Letters by frequency: \r\n";
+            foreach (Letter l in input)
+            {
+                return_text = return_text + l.letter + ": "
+                    + percentage(l.count * 100 / (file_contents.Length - file_contents.Split(' ').Length)) + "% || " 
+                    + l.count + " times total \r\n";
+            }
+            return return_text;
+        }
+
+
+        //Adds a "0" in front of the number if it is <10
+        private String percentage (int number)
+        {
+            if (number < 10)
+            {
+                return "0" + number;
+            }
+            return number.ToString();
+        }
+    }
+}
